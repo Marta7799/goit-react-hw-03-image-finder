d9e45ff19691ed5ce355b40086f1465f23a41f3a
Mart <marta.nikiel@yahoo.com>
2023-11-24 11:11:46 +0100

    changed loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,12 +2,12 @@ import { BallTriangle } from 'react-loader-spinner';
 import css from './Loader.module.css';
 
 export const Loader = () => (
-  <div className={css.Loader}>
+  <div className={css.loader}>
     <BallTriangle
       height={100}
       width={100}
       radius={5}
-      color="#4fa94d"
+      color="white"
       ariaLabel="ball-triangle-loading"
       wrapperClass={{}}
       wrapperStyle=""
