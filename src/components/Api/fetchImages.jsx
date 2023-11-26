9351d9bd9b9dac980190bd3c1aedc8acbfec8089
Mart <marta.nikiel@yahoo.com>
2023-11-26 12:05:28 +0100

    ready task 2

--- src/components/Api/fetchImages.jsx
+++ src/components/Api/fetchImages.jsx
@@ -1,21 +1,5 @@
 import axios from 'axios';
 
-// axios.defaults.baseURL = `https://pixabay.com/api`;
-
-// export const fetchImages = async (inputValue, pageNr) => {
-//   const response = await axios.get(
-//     `/?q=${inputValue}&page=${pageNr}&key=39809012-794bb9f85c23fb448d6e12ec5&image_type=photo&orientation=horizontal&per_page=12`
-//   );
-//   return response.data.hits.map(image => {
-//     return {
-//       id: image.id,
-//       webformatURL: image.webformatURL,
-//       largeImageURL: image.largeImageURL,
-//       tags: image.tags,
-//     };
-//   });
-// };
-
 const API_KEY = '39809012-794bb9f85c23fb448d6e12ec5';
 
 export const fetchImages = async (query, page, perPage) => {
